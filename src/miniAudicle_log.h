846f017de27939d371b76e26c68fae7310bbb6cc
Spencer Salazar <spencer@ccrma.stanford.edu>
2010-01-25 01:59:12 +0000

    - console flushing fix

---
+++ src/miniAudicle_log.h
@@ -0,0 +1,16 @@
+/*
+ *  miniAudicle_log.h
+ *  miniAudicle
+ *
+ *  Created by Spencer Salazar on 10/24/09.
+ *  Copyright 2009 Spencer Salazar. All rights reserved.
+ *
+ */
+
+#ifndef __MINIAUDICLE_LOG_H__
+#define __MINIAUDICLE_LOG_H__
+
+void mA_log(const char *, ...);
+void mA_log(int, const char *, ...);
+
+#endif // __MINIAUDICLE_LOG_H__
