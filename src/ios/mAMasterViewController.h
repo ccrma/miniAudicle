3a3f690f121c8f5297cdd0b7961f4e8e2e11eebe
Spencer Salazar <spencer@ccrma.stanford.edu>
2012-01-17 06:15:50 +0000

    - copyright info

--- src/ios/mAMasterViewController.h
+++ src/ios/mAMasterViewController.h
@@ -1,10 +1,26 @@
-//
-//  mAMasterViewController.h
-//  miniAudicle iOS
-//
-//  Created by Spencer Salazar on 12/20/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
-//
+/*----------------------------------------------------------------------------
+ miniAudicle iOS
+ iOS GUI to chuck audio programming environment
+ 
+ Copyright (c) 2005-2012 Spencer Salazar.  All rights reserved.
+ http://chuck.cs.princeton.edu/
+ http://soundlab.cs.princeton.edu/
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ U.S.A.
+ -----------------------------------------------------------------------------*/
 
 #import <UIKit/UIKit.h>
 
