1f5beddf65fd7138fa338a54ffa8e7267a8e1288
Spencer Salazar <spencer.salazar@gmail.com>
2013-06-13 21:58:13 -0700

    add examples in makefile

--- src/macosx/mAExampleBrowser.m
+++ src/macosx/mAExampleBrowser.m
@@ -8,6 +8,28 @@
 
 #import "mAExampleBrowser.h"
 
+
+@interface NSFileManager (isDirectory)
+
+- (BOOL)isDirectory:(NSString *)path;
+
+@end
+
+@implementation NSFileManager (isDirectory)
+
+- (BOOL)isDirectory:(NSString *)path
+{
+    BOOL isDirectory = NO;
+    if([self fileExistsAtPath:path isDirectory:&isDirectory])
+        return isDirectory;
+    else
+        return NO;
+}
+
+
+@end
+
+
 @interface mAExampleBrowser ()
 
 @end
@@ -37,8 +59,8 @@
 
 - (IBAction)open:(id)sender
 {
-//    [self.window close];
-
+    [self.window close];
+    
     NSString * examplePath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"examples"];
     NSString * columnPath = [examplePath stringByAppendingFormat:@"/%@", [_browser path]];
     [[NSDocumentController sharedDocumentController] openDocumentWithContentsOfURL:[NSURL fileURLWithPath:columnPath]
@@ -56,34 +78,41 @@
 
 - (NSInteger)browser:(NSBrowser *)sender numberOfRowsInColumn:(NSInteger)column
 {
+    NSFileManager * fileManager = [NSFileManager defaultManager];
+    
     NSString * examplePath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"examples"];
     NSString * columnPath = [examplePath stringByAppendingFormat:@"/%@", [sender pathToColumn:column]];
-    return [[[NSFileManager defaultManager] contentsOfDirectoryAtPath:columnPath error:nil] count];
+    NSArray * contents = [fileManager contentsOfDirectoryAtPath:columnPath error:nil];
+    
+    return [contents count];
 }
 
 - (void)browser:(NSBrowser *)sender willDisplayCell:(id)cell atRow:(NSInteger)row column:(NSInteger)column
 {
-    NSString * file;
-    NSString * fullpath;
+    NSBundle * mainBundle = [NSBundle mainBundle];
+    NSFileManager * fileManager = [NSFileManager defaultManager];
 
-    NSString * examplePath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"examples"];
+    NSString * examplePath = [[mainBundle resourcePath] stringByAppendingPathComponent:@"examples"];
     NSString * columnPath = [examplePath stringByAppendingFormat:@"/%@", [sender pathToColumn:column]];
-    NSArray * files = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:columnPath error:nil];
-    file = [files objectAtIndex:row];
-    fullpath = [columnPath stringByAppendingPathComponent:file];
+    NSArray * files = [fileManager contentsOfDirectoryAtPath:columnPath error:nil];
+    NSString * file = [files objectAtIndex:row];
+    NSString * fullpath = [columnPath stringByAppendingPathComponent:file];
     
     [cell setTitle:file];
-    
-    BOOL isDirectory = NO;
-    [[NSFileManager defaultManager] fileExistsAtPath:fullpath isDirectory:&isDirectory];
-    if(isDirectory)
+    [cell setEnabled:YES];
+    [cell setImage:nil];
+
+    if([fileManager isDirectory:fullpath])
     {
-        [cell setImage:[[NSBundle mainBundle] imageForResource:@"folder.png"]];
+        [cell setImage:[mainBundle imageForResource:@"folder.png"]];
         [cell setLeaf:NO];
     }
     else
     {
-        [cell setImage:[[NSBundle mainBundle] imageForResource:@"ckmini.png"]];
+        if([[file pathExtension] isEqualToString:@"ck"])
+            [cell setImage:[mainBundle imageForResource:@"ckmini.png"]];
+        else
+            [cell setEnabled:NO];
         [cell setLeaf:YES];
     }
 }
