22c94c03b37cabf94d744f48b5de671440f7e1e5
Spencer Salazar <spencer.salazar@gmail.com>
2013-07-28 01:57:26 -0700

    remove unnecessary override code from mAController

--- src/macosx/miniAudicleController.mm
+++ src/macosx/miniAudicleController.mm
@@ -311,41 +311,41 @@ const char* const MultiWindowDocumentControllerCloseAllContext = "com.samuelcart
 // The object is instantiated in Window.xib and becomes the application-global document
 // controller, overriding NSDocumentController‘s default instance.
 
-- (void)document:(NSDocument *)doc shouldClose:(BOOL)shouldClose contextInfo:(void  *)contextInfo
-{
-    if (contextInfo == MultiWindowDocumentControllerCloseAllContext) {
-//        NSLog(@"in close all. should close: %@",@(shouldClose));
-        if (shouldClose) {
-            // work on a copy of the window controllers array so that the doc can mutate its own array.
-            NSArray* windowCtrls = [doc.windowControllers copy];
-            for (NSWindowController* windowCtrl in windowCtrls) {
-                if ([windowCtrl respondsToSelector:@selector(removeDocument:)]) {
-                    [(id)windowCtrl removeDocument:doc];
-                }
-            }
-            
-            [windowCtrls release];
-            [doc close];
-            [self removeDocument:doc];
-        } else {
-            _didCloseAll = NO;
-        }
-    }
-}
+//- (void)document:(NSDocument *)doc shouldClose:(BOOL)shouldClose contextInfo:(void  *)contextInfo
+//{
+//    if (contextInfo == MultiWindowDocumentControllerCloseAllContext) {
+////        NSLog(@"in close all. should close: %@",@(shouldClose));
+//        if (shouldClose) {
+//            // work on a copy of the window controllers array so that the doc can mutate its own array.
+//            NSArray* windowCtrls = [doc.windowControllers copy];
+//            for (NSWindowController* windowCtrl in windowCtrls) {
+//                if ([windowCtrl respondsToSelector:@selector(removeDocument:)]) {
+//                    [(id)windowCtrl removeDocument:doc];
+//                }
+//            }
+//            
+//            [windowCtrls release];
+//            [doc close];
+//            [self removeDocument:doc];
+//        } else {
+//            _didCloseAll = NO;
+//        }
+//    }
+//}
 
 
 #pragma mark NSDocumentController
 
-- (void)closeAllDocumentsWithDelegate:(id)delegate didCloseAllSelector:(SEL)didCloseAllSelector contextInfo:(void *)contextInfo
-{
-//    NSLog(@"Closing all documents");
-    _didCloseAll = YES;
-    for (NSDocument* currentDocument in self.documents) {
-        [currentDocument canCloseDocumentWithDelegate:self shouldCloseSelector:@selector(document:shouldClose:contextInfo:) contextInfo:(void*)MultiWindowDocumentControllerCloseAllContext];
-    }
-    
-    objc_msgSend(delegate,didCloseAllSelector,self,_didCloseAll,contextInfo);
-}
+//- (void)closeAllDocumentsWithDelegate:(id)delegate didCloseAllSelector:(SEL)didCloseAllSelector contextInfo:(void *)contextInfo
+//{
+////    NSLog(@"Closing all documents");
+//    _didCloseAll = YES;
+//    for (NSDocument* currentDocument in self.documents) {
+//        [currentDocument canCloseDocumentWithDelegate:self shouldCloseSelector:@selector(document:shouldClose:contextInfo:) contextInfo:(void*)MultiWindowDocumentControllerCloseAllContext];
+//    }
+//    
+//    objc_msgSend(delegate,didCloseAllSelector,self,_didCloseAll,contextInfo);
+//}
 
 - (void)addDocument:(NSDocument *)doc
 {
