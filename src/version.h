13683a28055886c1c4ce5eac2da019f1719e736a
Spencer Salazar <spencer.salazar@gmail.com>
2013-10-24 02:39:29 -0700

    makefile fix and version bump

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
 
 #ifndef ENV_MA_VERSION
-#define ENV_MA_VERSION "1.3.0a"
+#define ENV_MA_VERSION "1.3.1-beta-1"
 #endif // ENV_MA_VERSION
--- src/version.mk
+++ src/version.mk
@@ -1,3 +1,3 @@
 
-PKG_VERSION?=1.3.2.0
-VERSION?=1.3.0a
+PKG_VERSION?=1.3.3.0-beta-1
+VERSION?=1.3.1-beta-1
