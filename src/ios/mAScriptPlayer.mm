a81c2e6c4e46af5a4b4988e6513edc965ec26978
Spencer Salazar <spencer.salazar@gmail.com>
2014-05-05 17:27:27 -0700

    partial refactor + functional looping

--- src/ios/mAScriptPlayer.mm
+++ src/ios/mAScriptPlayer.mm
@@ -30,11 +30,33 @@ static const t_CKFLOAT MAX_SHRED_TIMEOUT = 0.1;
 @end
 
 
+struct Shred
+{
+    Shred() : button(nil), timeout(0), timeoutMax(MAX_SHRED_TIMEOUT) { }
+    mAShredButton *button;
+    t_CKFLOAT timeout;
+    t_CKFLOAT timeoutMax;
+    
+    void reset() { timeout = 0; timeoutMax = MAX_SHRED_TIMEOUT; }
+};
+
+
+struct LoopShred
+{
+    t_CKINT loopCount;
+    string code;
+    string name;
+    string filepath;
+    vector<string> args;
+};
+
 
 @interface mAScriptPlayer ()
 {
-    map<t_CKUINT, mAShredButton *> _shredIdToButton;
-    map<t_CKUINT, t_CKFLOAT> _shredTimeouts;
+//    map<t_CKUINT, mAShredButton *> _shredIdToButton;
+//    map<t_CKUINT, t_CKFLOAT> _shredTimeouts;
+    map<t_CKUINT, Shred> _shreds; // shred id -> info
+    map<t_CKUINT, LoopShred> _loopShreds; // shred ids -> loop count
     t_CKFLOAT _lastTime;
     list<t_CKUINT> _shredOrder;
 }
@@ -155,7 +177,61 @@ static const t_CKFLOAT MAX_SHRED_TIMEOUT = 0.1;
 
 - (IBAction)loopShred:(id)sender
 {
-    NSLog(@"loop");
+    if(self.detailItem == nil) return;
+    
+    std::string code = [self.detailItem.text UTF8String];
+    std::string name = [self.detailItem.title UTF8String];
+    std::string filepath;
+    if(self.detailItem.path && [self.detailItem.path length])
+        filepath = [self.detailItem.path UTF8String];
+    vector<string> args;
+    t_CKUINT shred_id;
+    std::string output;
+    
+    t_OTF_RESULT otf_result = [mAChucKController chuckController].ma->run_code(code, name, args, filepath,
+                                                                               self.detailItem.docid,
+                                                                               shred_id, output);
+    
+    if( otf_result == OTF_SUCCESS )
+    {
+        //        if([self.windowController currentViewController] == self)
+        //            [[text_view textView] animateAdd];
+        //        self.textView.errorLine = -1;
+        //        self.textView.errorMessage = nil;
+        [self addShredButton:shred_id];
+        
+        _loopShreds[shred_id] = LoopShred();
+        _loopShreds[shred_id].loopCount = -1;
+        _loopShreds[shred_id].code = code;
+        _loopShreds[shred_id].name = name;
+        _loopShreds[shred_id].filepath = filepath;
+        _loopShreds[shred_id].args = args;
+    }
+    else if( otf_result == OTF_VM_TIMEOUT )
+    {
+        //        miniAudicleController * mac = [NSDocumentController sharedDocumentController];
+        //        [mac setLockdown:YES];
+    }
+    else if( otf_result == OTF_COMPILE_ERROR )
+    {
+        //        int error_line;
+        //        std::string result;
+        //        if( [mAChucKController chuckController].ma->get_last_result( self.detailItem.docid, NULL, &result, &error_line ) )
+        //        {
+        //            self.textView.errorLine = error_line;
+        //            self.textView.errorMessage = [NSString stringWithUTF8String:result.c_str()];
+        //        }
+        
+        //        if([self.windowController currentViewController] == self)
+        //            [[text_view textView] animateError];
+    }
+    else
+    {
+        //        if([self.windowController currentViewController] == self)
+        //            [[text_view textView] animateError];
+        //
+        //        [status_text setStringValue:[NSString stringWithUTF8String:result.c_str()]];
+    }
 }
 
 
@@ -247,8 +323,8 @@ static const t_CKFLOAT MAX_SHRED_TIMEOUT = 0.1;
     t_CKFLOAT deltaTime = (currentTime - _lastTime);
     
     list<t_CKUINT> removeList;
-    for(map<t_CKUINT, mAShredButton *>::iterator myShred = _shredIdToButton.begin();
-        myShred != _shredIdToButton.end(); myShred++)
+    for(map<t_CKUINT, Shred>::iterator myShred = _shreds.begin();
+        myShred != _shreds.end(); myShred++)
     {
         int shredId = myShred->first;
         
@@ -265,20 +341,77 @@ static const t_CKFLOAT MAX_SHRED_TIMEOUT = 0.1;
         }
         
         if(!foundIt)
-            _shredTimeouts[shredId] += (deltaTime);
-        if(_shredTimeouts[shredId] > MAX_SHRED_TIMEOUT)
+        {
+            _shreds[shredId].timeout += (deltaTime);
+        }
+        else
+        {
+            _shreds[shredId].timeout = 0;
+            _shreds[shredId].timeoutMax = 0;
+        }
+        
+        if(_shreds[shredId].timeout > _shreds[shredId].timeoutMax)
             removeList.push_back(shredId);
     }
     
     for(list<t_CKUINT>::iterator rm = removeList.begin(); rm != removeList.end(); rm++)
-        [self removeShredButton:*rm];
+    {
+        t_CKUINT shred_id = *rm;
+        if(_loopShreds.count(shred_id) && _loopShreds[shred_id].loopCount != 0)
+        {
+            /* continue looping */
+            
+            if(_loopShreds[shred_id].loopCount > 0)
+                _loopShreds[shred_id].loopCount--;
+            
+            t_CKUINT new_shred_id;
+            std::string output;
+            
+            t_OTF_RESULT otf_result = [mAChucKController chuckController].ma->run_code(_loopShreds[shred_id].code,
+                                                                                       _loopShreds[shred_id].name,
+                                                                                       _loopShreds[shred_id].args,
+                                                                                       _loopShreds[shred_id].filepath,
+                                                                                       self.detailItem.docid,
+                                                                                       new_shred_id, output);
+            
+            if( otf_result == OTF_SUCCESS )
+            {
+                if(new_shred_id != shred_id)
+                {
+                    // swap to new shred id (if necessary)
+                    _loopShreds[new_shred_id] = _loopShreds[shred_id];
+                    _loopShreds.erase(shred_id);
+                    _shreds[new_shred_id] = _shreds[shred_id];
+                    _shreds.erase(shred_id);
+                }
+                
+                _shreds[new_shred_id].reset();
+            }
+            else if( otf_result == OTF_VM_TIMEOUT )
+            {
+            }
+            else if( otf_result == OTF_COMPILE_ERROR )
+            {
+                _loopShreds.erase(shred_id);
+            }
+            else
+            {
+                _loopShreds.erase(shred_id);
+            }
+        }
+        else
+        {
+            /* remove shred from our list */
+            [self removeShredButton:shred_id];
+        }
+    }
     
     _lastTime = currentTime;
 }
 
 - (void)addShredButton:(t_CKUINT)shredId
 {
-    if(_shredIdToButton.count(shredId) == 0)
+    if(_shreds.count(shredId) == 0)
     {
         mAShredButton *shredView = [mAShredButton buttonWithType:UIButtonTypeCustom];
         float width = 25;
@@ -292,8 +425,9 @@ static const t_CKFLOAT MAX_SHRED_TIMEOUT = 0.1;
         
         
         [self.view addSubview:shredView];
-        _shredIdToButton[shredId] = shredView;
-        _shredTimeouts[shredId] = 0;
+        
+        _shreds[shredId] = Shred();
+        _shreds[shredId].button = shredView;
         _shredOrder.push_back(shredId);
         
         [shredView addTarget:self
@@ -304,11 +438,12 @@ static const t_CKFLOAT MAX_SHRED_TIMEOUT = 0.1;
 
 - (void)removeShredButton:(unsigned long)shredId
 {
-    if(_shredIdToButton.count(shredId) > 0)
+    if(_shreds.count(shredId) > 0)
     {
-        [_shredIdToButton[shredId] removeFromSuperview];
-        _shredIdToButton.erase(shredId);
-        _shredTimeouts.erase(shredId);
+        [_shreds[shredId].button removeFromSuperview];
+        _shreds.erase(shredId);
+        if(_loopShreds.count(shredId)) _loopShreds.erase(shredId);
+        
         BOOL relayout = NO;
         if(_shredOrder.back() != shredId)
             relayout = YES;
@@ -323,7 +458,7 @@ static const t_CKFLOAT MAX_SHRED_TIMEOUT = 0.1;
     int idx = 0;
     for(list<t_CKUINT>::iterator i = _shredOrder.begin(); i != _shredOrder.end(); i++)
     {
-        mAShredButton *shredView = _shredIdToButton[*i];
+        mAShredButton *shredView = _shreds[*i].button;
         float width = 25;
         int xIdx = idx/2;
         int yIdx = idx%2;
