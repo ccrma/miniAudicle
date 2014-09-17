d332a7a8433345f506bf2523d518180e425505d8
Spencer Salazar <spencer.salazar@gmail.com>
2014-09-17 18:47:40 +0300

    motion accelerometer

---
+++ src/ios/ulib_motion.h
@@ -0,0 +1,14 @@
+//
+//  ulib_motion.h
+//  miniAudicle
+//
+//  Created by Spencer Salazar on 9/17/14.
+//
+//
+
+#ifndef __miniAudicle__ulib_motion__
+#define __miniAudicle__ulib_motion__
+
+#include <iostream>
+
+#endif /* defined(__miniAudicle__ulib_motion__) */
---
+++ src/ios/ulib_motion.mm
@@ -0,0 +1,9 @@
+//
+//  ulib_motion.cpp
+//  miniAudicle
+//
+//  Created by Spencer Salazar on 9/17/14.
+//
+//
+
+#include "ulib_motion.h"
