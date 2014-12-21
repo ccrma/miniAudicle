dc7f41b281fc25da1495001e37048baf4001ebba
Spencer Salazar <spencer.salazar@gmail.com>
2014-12-20 21:00:09 -0800

    version bump

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
 
 #ifndef ENV_MA_VERSION
-#define ENV_MA_VERSION "1.3.2-rc-1"
+#define ENV_MA_VERSION "1.3.2-rc-2"
 #endif // ENV_MA_VERSION
--- src/version.mk
+++ src/version.mk
@@ -1,3 +1,3 @@
 
-PKG_VERSION?=1.3.5.0-rc-1
-VERSION?=1.3.2-rc-1
+PKG_VERSION?=1.3.5.0-rc-2
+VERSION?=1.3.2-rc-2
