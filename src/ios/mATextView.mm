cc6ee0af40bd6885d6b6f048aada994840cc6dcc
Spencer Salazar <spencer.salazar@gmail.com>
2015-01-04 18:06:41 -0800

    line numbers

--- src/ios/mATextView.mm
+++ src/ios/mATextView.mm
@@ -8,14 +8,26 @@
 
 #import "mATextView.h"
 
+#define LINENUM_LEFTMARGIN (8)
+#define LINENUM_RIGHTMARGIN (8)
+
 
 @interface mATextView ()
 {
     UIView *_imageView;
+    
+    float _lineNumbersWidth;
+    float _lineNumbersHeightPerLine;
 }
 
+@property (nonatomic, strong) UIFont *lineNumberFont;
+
+- (void)updateLineNumbers;
 - (void)animateImage:(UIImage *)image atLocation:(CGPoint)location;
 - (void)animateImage:(UIImage *)image atLocation:(CGPoint)location size:(CGSize)size;
+
+- (void)drawLineNumbersInRect:(CGRect)rect;
+- (void)drawLineNumber:(int)num forLineRect:(CGRect)rect;
 
 @end
 
@@ -35,13 +47,22 @@
     [self setNeedsDisplay];
 }
 
+- (void)initCommon
+{
+    // Initialization code
+    self.errorLine = -1;
+    self.contentMode = UIViewContentModeRedraw;
+    _lineNumbersWidth = 0;
+    self.lineNumberFont = [UIFont fontWithName:@"Menlo" size:10];
+    [self updateLineNumbers];
+    _lineNumbersHeightPerLine = self.font.lineHeight;
+}
+
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
     if (self) {
-        // Initialization code
-        self.errorLine = -1;
-        self.contentMode = UIViewContentModeRedraw;
+        [self initCommon];
     }
     return self;
 }
@@ -51,23 +72,40 @@
 {
     self = [super initWithCoder:aDecoder];
     if (self) {
-        // Initialization code
-        self.errorLine = -1;
-        self.contentMode = UIViewContentModeRedraw;
+        [self initCommon];
     }
     return self;
 }
 
 - (NSDictionary *)errorTextAttributes
 {
-    return [NSDictionary dictionaryWithObjectsAndKeys:
-            [UIFont fontWithName:@"Menlo" size:12], NSFontAttributeName,
-            nil];
+    return @{ NSFontAttributeName: [UIFont fontWithName:@"Menlo" size:12] };
 }
 
 - (UIColor *)errorColor
 {
     return [UIColor colorWithRed:1 green:0.45 blue:0.45 alpha:1];
+}
+
+- (NSDictionary *)lineNumberTextAttributes
+{
+    NSMutableParagraphStyle *paragraphStyle = [NSMutableParagraphStyle new];
+    paragraphStyle.alignment = NSTextAlignmentRight;
+    return @{ NSFontAttributeName: self.lineNumberFont,
+              NSParagraphStyleAttributeName: paragraphStyle,
+              NSForegroundColorAttributeName: [UIColor colorWithWhite:0.5 alpha:1] };
+}
+
+- (void)updateLineNumbers
+{
+    float lineNumbersWidth = LINENUM_LEFTMARGIN + [@"00000" sizeWithAttributes:[self lineNumberTextAttributes]].width + LINENUM_RIGHTMARGIN;
+    if(_lineNumbersWidth != lineNumbersWidth)
+    {
+        _lineNumbersWidth = lineNumbersWidth;
+        UIEdgeInsets insets = self.textContainerInset;
+        insets.left += _lineNumbersWidth;
+        self.textContainerInset = insets;
+    }
 }
 
 - (void)animateImage:(UIImage *)image atLocation:(CGPoint)location
@@ -134,6 +172,8 @@
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    
     if(self.errorLine >= 1)
     {
         CGFloat yBufferEdge = 1;
@@ -149,8 +189,6 @@
         CGFloat errorMsgBoxYStart;
         CGFloat errorMsgBoxXSize;
         CGFloat errorMsgBoxYSize;
-        
-        CGContextRef ctx = UIGraphicsGetCurrentContext();
         
 //        [[self errorColor] set];
 //        CGContextFillPath(ctx);
@@ -199,6 +237,68 @@
 
         [super drawRect:rect];
     }
+    
+    /* draw gutter */
+    
+    CGRect ixRect = CGRectIntersection(rect, CGRectMake(self.contentOffset.x, self.contentOffset.y, _lineNumbersWidth, self.bounds.size.height));
+    [[UIColor colorWithWhite:0.83 alpha:1] set];
+    CGContextFillRect(ctx, ixRect);
+    
+//    int lineStart = floorf(rect.origin.y/_lineNumbersHeightPerLine);
+//    int lineEnd = ceilf((rect.origin.y+rect.size.height)/_lineNumbersHeightPerLine);
+    
+    [self drawLineNumbersInRect:rect];
+}
+
+- (void)drawLineNumbersInRect:(CGRect)rect
+{
+    NSLayoutManager *layoutManager = self.layoutManager;
+    NSTextContainer *textContainer = self.textContainer;
+    
+    // Find the glyph range for the visible glyphs
+    NSRange glyphRange = [layoutManager glyphRangeForBoundingRect:rect inTextContainer:textContainer];
+    
+    // Calculate the start and end indexes for the glyphs
+    unsigned startGlyphIndex = glyphRange.location;
+    unsigned endGlyphIndex = glyphRange.location + glyphRange.length;
+    
+    int index = 0;
+    int lineNumber = 1;
+    
+    // Skip all lines that are visible at the top of the text view (if any)
+    NSRange lineRange;
+    CGRect lineRect;
+    while(index < startGlyphIndex)
+    {
+        lineRect = [layoutManager lineFragmentRectForGlyphAtIndex:index effectiveRange:&lineRange];
+        index = NSMaxRange( lineRange );
+        ++lineNumber;
+    }
+    
+    for(index = startGlyphIndex; index < endGlyphIndex; lineNumber++)
+    {
+        lineRect = [layoutManager lineFragmentRectForGlyphAtIndex:index effectiveRange:&lineRange];
+        index = NSMaxRange(lineRange);
+        [self drawLineNumber:lineNumber forLineRect:lineRect];
+    }
+    
+    lineRect = [layoutManager extraLineFragmentRect];
+    if(lineRect.size.width != 0 && lineRect.size.height != 0)
+    {
+        [self drawLineNumber:lineNumber forLineRect:lineRect];
+    }
+}
+
+- (void)drawLineNumber:(int)num forLineRect:(CGRect)lineRect
+{
+    NSString *lineString = [NSString stringWithFormat:@"%i", num];
+    
+    CGRect numberRect = lineRect;
+    numberRect.origin.y += self.textContainerInset.top + (self.font.lineHeight-self.lineNumberFont.lineHeight)*0.9;
+    numberRect.origin.x = LINENUM_LEFTMARGIN;
+    numberRect.size.width = _lineNumbersWidth-(LINENUM_LEFTMARGIN+LINENUM_RIGHTMARGIN);
+    [lineString drawInRect:numberRect
+            withAttributes:[self lineNumberTextAttributes]];
 }
 
 @end
