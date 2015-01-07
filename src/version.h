9abb509c517376982d5622f69d5b6b6d384bb454
Spencer Salazar <spencer.salazar@gmail.com>
2015-01-06 21:59:47 -0800

    upversioned chuck and version bump

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
 
 #ifndef ENV_MA_VERSION
-#define ENV_MA_VERSION "1.3.3"
+#define ENV_MA_VERSION "1.3.4-beta-1"
 #endif // ENV_MA_VERSION
--- src/version.mk
+++ src/version.mk
@@ -1,3 +1,3 @@
 
-PKG_VERSION?=1.3.5.0
-VERSION?=1.3.3
+PKG_VERSION?=1.3.5.1-beta-1
+VERSION?=1.3.4-beta-1
